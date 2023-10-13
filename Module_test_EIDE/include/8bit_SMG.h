225e6028dfa49d804aea820b12111679436ff3b5
CWndpkj <131755559+CWndpkj@users.noreply.github.com>
2023-10-14 00:04:23 +0800

    Delete Module_test_EIDE directory

--- Module_test_EIDE/include/8bit_SMG.h
+++
@@ -1,6 +0,0 @@
-#ifndef _8BIT_SMG_H_
-#define _8BIT_SMG_H_
-#include "delay.h"
-
-void SMG_Show(unsigned int _data);
-#endif
--- Module_test_EIDE/include/delay.h
+++
@@ -1,8 +0,0 @@
-#ifndef _DELAY_H_
-#define _DELAY_H_
-//延时函数
-/*
-延时n毫秒
-*/
-void delayms(unsigned int declipse); 
-#endif
