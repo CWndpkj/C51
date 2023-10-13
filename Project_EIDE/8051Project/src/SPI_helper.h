cd344cbd4f046de4a2bf40d67bd14a0ebe93b067
CWndpkj <131755559+CWndpkj@users.noreply.github.com>
2023-10-14 00:04:53 +0800

    Delete Project_EIDE/8051Project directory

--- Project_EIDE/8051Project/src/SPI_helper.h
+++
@@ -1,6 +0,0 @@
-#ifndef _SPI_HELPER_H_
-#define _SPI_HELPER_H_
-#include <REGX52.H>
-
-
-#endif
--- Project_EIDE/8051Project/src/key.h
+++
@@ -1,7 +0,0 @@
-#ifndef _KEY_H_
-#define _KEY_H_
-
-//返回按下的键值，范围1-4,返回0时没有按键被按下
-unsigned char Get_Key();
-
-#endif
