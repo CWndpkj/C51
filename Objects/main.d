a5f3f4f8c6d76d8425fed6be5dfbf7bfaa2a795d
CWndpkj <131755559+CWndpkj@users.noreply.github.com>
2023-10-14 00:05:01 +0800

    Delete Objects directory

--- Objects/main.d
+++
@@ -1,8 +0,0 @@
-./objects/main.o: main.c \
-  D:\install\Keil\Pack\Keil\STM32F1xx_DFP\2.4.1\Device\Include\stm32f10x.h \
-  D:\install\Keil\Pack\ARM\CMSIS\5.9.0\CMSIS\Core\Include\core_cm3.h \
-  D:\install\Keil\ARM\ARMCLANG\Bin\..\include\stdint.h \
-  D:\install\Keil\Pack\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_version.h \
-  D:\install\Keil\Pack\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_compiler.h \
-  D:\install\Keil\Pack\ARM\CMSIS\5.9.0\CMSIS\Core\Include\cmsis_armclang.h \
-  D:\install\Keil\Pack\Keil\STM32F1xx_DFP\2.4.1\Device\Include\system_stm32f10x.h
